e107e4c4fcce28b8b545921f6e8dc72596b75f86
coffeebinge <coffeebinge@gmail.com>
2024-03-15 20:18:06 -0700

    added each device type to render

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python
 import jinja2
 import yaml
-import sys
+
 
 
 loader = jinja2.FileSystemLoader(searchpath="./jinja")
@@ -17,3 +17,25 @@ border = jenv.get_template("jinja/border.j2")
 ## define input params separating variables by group.. GLOBAL, CREDS, USERINPUT. 
 ## render template config output into ./config directory
 ## render leaves, core/spine and border leaves separately
+
+with open('access.list') as leafinv:
+    leaves = leafinv.readlines()
+with open('spine.list') as spineinv:
+    spines = spineinv.readlines()
+with open('core.list') as coreinv:
+    cores = coreinv.readlines()
+with open('border.list') as borderinv:
+    borders = borderinv.readlines()
+
+with open('global-parameters.yml') as global_yml:
+    global_vars = yaml.safe_load(global_yml)
+with open('credentials.yml') as cred_yml:
+    cred_vars = yaml.safe_load(cred_yml)
+
+for leaf in leaves:
+    leaf = leaf.replace('\n', '')
+    with open(leaf+'.yml') as leaf_yml:
+        leaf_vars = yaml.safe_load(leaf_yml)
+        f = open('configs/'+leaf+'.conf', 'w')
+        print(access.render(GLOBAL=global_vars, CREDS=cred_vars, USERINPUT=leaf_vars), file=f)
+        f.close()
