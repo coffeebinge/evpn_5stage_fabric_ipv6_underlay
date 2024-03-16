f8798b7d7b15003ab8bcb4aba3ec5c21fde1c691
coffeebinge <coffeebinge@gmail.com>
2024-03-15 20:26:36 -0700

    ok ok, the spine, core and border were auto generated in like 5 seconds based on the leaf

--- generate.py
+++ generate.py
@@ -1,8 +1,11 @@
 #!/user/bin/env python
 import jinja2
 import yaml
+import os
 
 
+if not os.path.exists('configs'):
+    os.makedirs('configs')
 
 loader = jinja2.FileSystemLoader(searchpath="./jinja")
 jenv = jinja2.Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
@@ -17,6 +20,8 @@ border = jenv.get_template("jinja/border.j2")
 ## define input params separating variables by group.. GLOBAL, CREDS, USERINPUT. 
 ## render template config output into ./config directory
 ## render leaves, core/spine and border leaves separately
+## fyi: github copilot is amazing! It took the above 4 comments and ran with it... 
+## I barely had to type more than a few letters
 
 with open('access.list') as leafinv:
     leaves = leafinv.readlines()
@@ -39,3 +44,27 @@ for leaf in leaves:
         f = open('configs/'+leaf+'.conf', 'w')
         print(access.render(GLOBAL=global_vars, CREDS=cred_vars, USERINPUT=leaf_vars), file=f)
         f.close()
+
+for spine in spines:
+    spine = spine.replace('\n', '')
+    with open(spine+'.yml') as spine_yml:
+        spine_vars = yaml.safe_load(spine_yml)
+        f = open('configs/'+spine+'.conf', 'w')
+        print(spine.render(GLOBAL=global_vars, CREDS=cred_vars, USERINPUT=spine_vars), file=f)
+        f.close()
+
+for core in cores:
+    core = core.replace('\n', '')
+    with open(core+'.yml') as core_yml:
+        core_vars = yaml.safe_load(core_yml)
+        f = open('configs/'+core+'.conf', 'w')
+        print(core.render(GLOBAL=global_vars, CREDS=cred_vars, USERINPUT=core_vars), file=f)
+        f.close()
+
+for border in borders:
+    border = border.replace('\n', '')
+    with open(border+'.yml') as border_yml:
+        border_vars = yaml.safe_load(border_yml)
+        f = open('configs/'+border+'.conf', 'w')
+        print(border.render(GLOBAL=global_vars, CREDS=cred_vars, USERINPUT=border_vars), file=f)
+        f.close()   
